61205d9553af3efd0f6525752df310e0f3e4a64f
onlineasd2 <mailonline95@gmail.com>
2024-10-22 16:32:51 +0500

    main_layout_1

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <main>
         <section className="section p-12">
-          <header className='header flex justify-center relative'>
+          <header className='header flex justify-center items-center relative mb-8'>
             <button className="header__button_add p-0 border-none absolute top-0 left-0"><img src="./btn-add.svg" className='w-10 h-10' alt="" /></button>
             <div className="title flex flex-col justify-center items-center">
               <img className='w-12 h-12' src="./rocket-lunch.svg" alt="" />
@@ -22,7 +22,7 @@ function App() {
             <div className="list__item flex items-center justify-between p-4 w-full bg-white rounded-md drop-shadow-md">
               <div className="list__title text-left w-9/12">
                 <h3 className='text-2xl font-bold'>Задача 1</h3>
-                <p className='truncate w-full'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <p className='truncate w-full text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
               </div>
               <div className="list__buttons flex">
                 <button className='list__button-edit w-20 bg-transparent'><img src="./btn-edit.svg" /></button>
