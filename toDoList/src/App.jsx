d2fdf42fe46c2811cf5d2e41063aeda69cc56d11
onlineasd2 <mailonline95@gmail.com>
2024-10-23 14:33:41 +0500

    get_request

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -11,8 +11,9 @@ function App() {
     <>
       <main>
         <section className="section p-12">
+          
           <header className='header flex justify-center items-center relative mb-8'>
-            <button className="header__button_add p-0 border-none absolute top-0 left-0"><img src="./btn-add.svg" className='w-10 h-10' alt="" /></button>
+            <button className="header__button_add p-0 border-none absolute top-0 left-4"><img src="./btn-add.svg" className='w-10 h-10' alt="" /></button>
             <div className="title flex flex-col justify-center items-center">
               <img className='w-12 h-12' src="./rocket-lunch.svg" alt="" />
               <h2 className='text-4xl font-bold'>Список Задач</h2>
--- toDoList/src/components/list.jsx
+++ toDoList/src/components/list.jsx
@@ -1,27 +1,33 @@
-//import { useState } from 'react'
+import React from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 
 import axios from "axios"
 
 function List() {
-  //const [count, setCount] = useState(0)
-  
+    const [tasks, setTasks] = React.useState();
+    React.useEffect(() => {
+        axios.get("https://671796f7b910c6a6e0290314.mockapi.io/tasks").then((res) => {
+            setTasks(res.data);
+            console.log(res.data);
+        });
+    }, []);
+
   return (
     <>
         <div className="list flex justify-center flex-col gap-6 max-w-6xl">
-
-            <div className="list__item flex items-center justify-between p-4 w-full bg-white rounded-md drop-shadow-md">
+            {tasks?.map((task) => (
+            <div key={task.id} className="cursor-pointer list__item flex items-center justify-between p-4 w-full bg-white rounded-md drop-shadow-md">
                 <div className="list__title text-left w-9/12">
-                    <h3 className='text-2xl font-bold'>Задача 1</h3>
-                    <p className='truncate w-full text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    <h3 className='text-2xl font-bold'>{task.title}</h3>
+                    <p className='truncate w-full text-gray-600'>{task.description}</p>
                 </div>
                 <div className="list__buttons flex">
                     <button className='list__button-edit w-20 bg-transparent'><img src="./btn-edit.svg" /></button>
                     <button className='list__button-delete w-20 bg-transparent'><img src="./btn-delete.svg" /></button>
                 </div>
             </div>
-
+            ))}
         </div>
     </>
   )
